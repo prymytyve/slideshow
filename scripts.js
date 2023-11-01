ae0b575653f80a681a85b6c234424a7b0e0eb9f1
Jeremy <126707817+prymytyve@users.noreply.github.com>
2023-10-31 21:00:11 -0400

    Add more images to slideshow

--- scripts.js
+++ scripts.js
@@ -35,9 +35,40 @@ const imageObjArr = [
   },
   {
     imageSrc: "./img/2.jpg",
-    credit: "Photo by: Joshua Kettle",
+    credit: "Photo by:",
   },
-  //{ imageSrc:, credit:}
+  {
+    imageSrc: "./img/3.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/4.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/5.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/6.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/7.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/8.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/9.jpg",
+    credit: "Photo by:",
+  },
+  {
+    imageSrc: "./img/10.jpg",
+    credit: "Photo by:",
+  },
 ];
 
 for (let i = 0; i < imageObjArr.length; i++) {
@@ -46,3 +77,11 @@ for (let i = 0; i < imageObjArr.length; i++) {
 
 //Photo by <a href="https://unsplash.com/@maeva_vgr?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Maëva Vigier</a> on <a href="https://unsplash.com/photos/a-train-traveling-down-train-tracks-next-to-a-tall-building-Ti8E5_ZoP8s?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>'
 //Photo by <a href="https://unsplash.com/@joshuakettle?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Joshua Kettle</a> on <a href="https://unsplash.com/photos/a-mountain-range-covered-in-snow-under-a-cloudy-sky-f1Ve8K7y7NQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@jswords?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jack Swords</a> on <a href="https://unsplash.com/photos/an-aerial-view-of-a-winding-road-near-a-beach-v5-TLuYsOsk?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@neom?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">NEOM</a> on <a href="https://unsplash.com/photos/a-view-of-a-mountain-range-in-the-desert-t3dlLOhdEzs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@jersey_photos?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Travis Leery</a> on <a href="https://unsplash.com/photos/a-lighthouse-sitting-on-top-of-a-rocky-beach-under-a-cloudy-sky-mEzCmIg_OU0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@kristapsungurs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kristaps Ungurs</a> on <a href="https://unsplash.com/photos/an-aerial-view-of-a-forest-with-lots-of-trees-PMIYom6wzAc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@martys111?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Martin Marek</a> on <a href="https://unsplash.com/photos/a-view-of-a-mountain-range-covered-in-fog--y9T0XDEUYo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@joshuakettle?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Joshua Kettle</a> on <a href="https://unsplash.com/photos/a-view-of-a-mountain-with-a-very-tall-peak-PhJS-gbFpLE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@jersey_photos?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Travis Leery</a> on <a href="https://unsplash.com/photos/the-sun-is-setting-over-the-water-on-the-beach-GZJGPB2QsbY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+//Photo by <a href="https://unsplash.com/@valentinbalan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Valentin Balan</a> on <a href="https://unsplash.com/photos/a-group-of-rock-formations-in-the-desert-FkKuf0airVQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
